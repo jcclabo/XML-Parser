d655246d19876224c180de1d1f924e2c5b409f1e
James Clabo <90985427+jcclabo@users.noreply.github.com>
2025-07-01 15:14:59 -0400

    Update xmlparser.h
    Reordered to match xmlparser.c

--- xmlparser.h
+++ xmlparser.h
@@ -15,40 +15,40 @@ typedef int callback(char*, char*, char*);
 
 #define MAX_COMMENT_SIZE 12345
 
-int no_op_callback(char*, char*, char*);
-
-char parse_entity(char*, FILE*, char*, callback*);
-
-char* parse_comment(char*, char* comment, FILE*, char*, callback*);
-
-int report_comment(char*, FILE*, char*, callback*);
-
-int report_any_comments(char*, FILE*, char*, callback*);
-
-int parse_el_name(char*, El*, FILE*, callback*);
-
-int parse_el_value(char*, El*, FILE*, callback*);
-
-int parse_attr_name(char*, char*, FILE*, char*, callback*);
-
-int parse_attr_value(char*, char*, FILE*, char*, callback*);
-
-int unique_attr_check(El*, int, callback*);
-
-int parse_attrs(char*, El*, FILE*, callback*);
-
-int alloc_attr_arr(El*, int, callback*);
-
-int alloc_el_arr(El*, int, callback*);
-
-int parse_open_el(char*, El*, FILE*, callback*);
-
-int parse_el_content(char*, El*, FILE*, callback*);
-
-int parse_close_el(char* c, El*, FILE*, callback*);
+El* parse_file(char*, callback*);
 
 El* parse_el(char*, FILE*, char*, callback*);
 
-El* parse_file(char*, callback*);
+int parse_close_el(char* c, El*, FILE*, callback*);
+
+int parse_el_content(char*, El*, FILE*, callback*);
+
+int parse_open_el(char*, El*, FILE*, callback*);
+
+int alloc_el_arr(El*, int, callback*);
+
+int alloc_attr_arr(El*, int, callback*);
+
+int parse_attrs(char*, El*, FILE*, callback*);
+
+int unique_attr_check(El*, int, callback*);
+
+int parse_attr_value(char*, char*, FILE*, char*, callback*);
+
+int parse_attr_name(char*, char*, FILE*, char*, callback*);
+
+int parse_el_value(char*, El*, FILE*, callback*);
+
+int parse_el_name(char*, El*, FILE*, callback*);
+
+int report_any_comments(char*, FILE*, char*, callback*);
+
+int report_comment(char*, FILE*, char*, callback*);
+
+char* parse_comment(char*, char* comment, FILE*, char*, callback*);
+
+int no_op_callback(char*, char*, char*);
+
+char parse_entity(char*, FILE*, char*, callback*);
 
 #endif
